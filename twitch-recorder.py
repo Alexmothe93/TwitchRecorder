89e3cf1aa38a9f11040ea0792e8cf542e1ee098a
Alexmothe93 <alexmothe@hotmail.fr>
2019-07-07 18:28:44 +0200

    Version 0.5
    - Replace all print() functions by logging
    - Add option to enable verbose mode (debug)
    - Twitch Kraken API does not make the difference between offline and not found streamer, code adapted

--- twitch-recorder.py
+++ twitch-recorder.py
@@ -5,6 +5,7 @@
 # Version 0.3 : Change watch() to check recorder before streamer, fix config file argument, less verbosity
 # Version 0.4 : Add WindowsInhibitor class to prevent Windows to sleep/hibernate when recording, used refresh argument in watch mode to set time between 2 checks when recorder is alive, fixed some syntax faults
 # Version 0.4.1 : Watcher mode: waiting in seconds, not minutes...
+# Version 0.5 : Add logging features, verbose mode and update streamer status according to Twitch Kraken API returns
 
 import requests
 import os
@@ -14,53 +15,53 @@ import sys
 import subprocess
 import datetime
 import configargparse
+import logging
 
 class WindowsInhibitor:
-    '''Prevent OS sleep/hibernate in windows; code from:
-    https://github.com/h3llrais3r/Deluge-PreventSuspendPlus/blob/master/preventsuspendplus/core.py
-    API documentation:
-    https://msdn.microsoft.com/en-us/library/windows/desktop/aa373208(v=vs.85).aspx'''
-    ES_CONTINUOUS = 0x80000000
-    ES_SYSTEM_REQUIRED = 0x00000001
+	'''Prevent OS sleep/hibernate in windows; code from:
+	https://github.com/h3llrais3r/Deluge-PreventSuspendPlus/blob/master/preventsuspendplus/core.py
+	API documentation:
+	https://msdn.microsoft.com/en-us/library/windows/desktop/aa373208(v=vs.85).aspx'''
+	ES_CONTINUOUS = 0x80000000
+	ES_SYSTEM_REQUIRED = 0x00000001
 
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def inhibit(self):
-        import ctypes
-        print("Preventing Windows from going to sleep")
-        ctypes.windll.kernel32.SetThreadExecutionState(
-            WindowsInhibitor.ES_CONTINUOUS | \
-            WindowsInhibitor.ES_SYSTEM_REQUIRED)
+	def inhibit(self):
+		import ctypes
+		logging.info("Preventing Windows from going to sleep.")
+		ctypes.windll.kernel32.SetThreadExecutionState(
+			WindowsInhibitor.ES_CONTINUOUS | \
+			WindowsInhibitor.ES_SYSTEM_REQUIRED)
 
-    def uninhibit(self):
-        import ctypes
-        print("Allowing Windows to go to sleep")
-        ctypes.windll.kernel32.SetThreadExecutionState(
-            WindowsInhibitor.ES_CONTINUOUS)
+	def uninhibit(self):
+		import ctypes
+		logging.info("Allowing Windows to go to sleep.")
+		ctypes.windll.kernel32.SetThreadExecutionState(
+			WindowsInhibitor.ES_CONTINUOUS)
 
 class TwitchRecorder:
 
 	def __init__(self):
 		self.clientID = "jzkbprff40iqj646a697cyrvl0zt2m6" #Client ID of Twitch website
-		self.version = "0.4" #To increment to each modification
+		self.version = "0.5" #To increment to each modification
 		self.osSleep = None
 		if os.name == 'nt':
 			self.osSleep = WindowsInhibitor()
 
-	
 	def run(self):
 		# make sure the interval to check user availability is not less than 15 seconds
 		if(self.refresh < 15):
-			print("Check interval should not be lower than 15 seconds, set check interval to 15 seconds.")
+			logging.warning("Check interval should not be lower than 15 seconds, set check interval to 15 seconds.")
 			self.refresh = 15
-		
+
 		if self.mode == "recorder":
 			self.record()
 		elif self.mode == "watcher":
 			self.watch()
 		else:
-			print("Mode not recognized, exiting.")
+			logging.error("Mode not recognized, exiting.")
 
 	def record(self):
 		# path to recording stream
@@ -85,63 +86,59 @@ class TwitchRecorder:
 			try:
 				videoList = [f for f in os.listdir(self.recordedPath) if os.path.isfile(os.path.join(self.recordedPath, f))]
 				if(len(videoList) > 0):
-					print('Fixing previously recorded files.')
+					logging.info("Fixing previously recorded files.")
 				for f in videoList:
 					recordedFilename = os.path.join(self.recordedPath, f)
-					print('Fixing ' + recordedFilename + '.')
+					logging.info("Fixing "+recordedFilename+".")
 					try:
 						subprocess.call([self.ffmpegPath, '-err_detect', 'ignore_err', '-i', recordedFilename, '-c', 'copy', os.path.join(self.processedPath,f)])
 						os.remove(recordedFilename)
 					except Exception as e:
-						print(e)
+						logging.error(e)
 			except Exception as e:
-				print(e)
+				logging.error(e)
 
-		print(self.version, "- Checking for", self.streamer, "every", self.refresh, "seconds. Record with", self.quality, "quality.")
+		logging.info("Checking for "+self.streamer+" every "+str(self.refresh)+" seconds. Record with "+self.quality+" quality.")
 		self.loopcheck()
 
 	def watch(self):
 		while True:
 			if self.recorderAlive():
-				print(datetime.datetime.now().strftime("%Hh%Mm%Ss"),"Recorder is alive, waiting", self.refresh, "seconds.")
+				logging.info("Recorder is alive, waiting "+str(self.refresh)+" seconds.")
 				time.sleep(self.refresh)
 			else:
-				print(datetime.datetime.now().strftime("%Hh%Mm%Ss"),"Recorder is sleeping, checking if", self.streamer, "is online.")
+				logging.info("Recorder is sleeping, checking if "+self.streamer+" is online.")
 				status, info = self.checkStreamer()
 				if status == 0:
-					print(self.streamer, "is online.")
+					logging.info(self.streamer+" is online.")
 					self.wakeRecorder()
 				elif status == 1:
-					print(self.version, "-", self.streamer, "currently offline, checking again in", self.refresh, "seconds.")
-					time.sleep(self.refresh)
-				elif status == 2:
-					print("Streamer not found.")
+					logging.info(self.streamer+" currently offline or not found, checking again in "+str(self.refresh)+" seconds.")
 					time.sleep(self.refresh)
 				else:
-					print(datetime.datetime.now().strftime("%Hh%Mm%Ss"), "unexpected error. Will try again in 15 seconds.")
+					logging.error("Unexpected error. Will try again in 15 seconds.")
 					time.sleep(15)
-		
+
 	def recorderAlive(self):
 		if os.system("ping -c 1 " + self.recorderIPAddress + " > /dev/null") == 0:
 			return True
 		else:
 			return False
-	
+
 	def wakeRecorder(self):
-		print("Wake recorder...")
+		logging.info("Wake recorder...")
 		subprocess.call(["wakeonlan", self.recorderMACAddress])
 		while self.recorderAlive() == False:
-			print("Waiting for the recorder to wake up...")
-		print("The recorder is awake.")
-		
+			logging.info("Waiting for the recorder to wake up...")
+		logging.info("The recorder is awake.")
+
 	def checkStreamer(self):
-		# 0: online, 
-		# 1: offline, 
-		# 2: not found, 
-		# 3: error
+		# 0: Online
+		# 1: Offline or not found
+		# 2: Error
 		url = 'https://api.twitch.tv/kraken/streams/' + self.streamer
 		info = None
-		status = 3
+		status = 2
 		try:
 			r = requests.get(url, headers = {"Client-ID" : self.clientID}, timeout = 15)
 			r.raise_for_status()
@@ -151,9 +148,8 @@ class TwitchRecorder:
 			else:
 				status = 0
 		except requests.exceptions.RequestException as e:
-			if e.response:
-				if e.response.reason == 'Not Found' or e.response.reason == 'Unprocessable Entity':
-					status = 2
+			logging.error("An error has occurred when trying to get streamer status.")
+			logging.debug(e)
 
 		return status, info
 
@@ -161,16 +157,13 @@ class TwitchRecorder:
 		while True:
 			status, info = self.checkStreamer()
 			if status == 2:
-				print("Streamer not found.")
-				time.sleep(self.refresh)
-			elif status == 3:
-				print(datetime.datetime.now().strftime("%Hh%Mm%Ss"), "unexpected error. Will try again in 15 seconds.")
+				logging.error("Unexpected error. Will try again in 15 seconds.")
 				time.sleep(15)
 			elif status == 1:
-				print(self.version, "-", self.streamer, "currently offline, checking again in", self.refresh, "seconds.")
+				logging.info(self.streamer+" currently offline or not found, checking again in "+str(self.refresh)+" seconds.")
 				time.sleep(self.refresh)
 			elif status == 0:
-				print(self.streamer, "online. Stream recording in session.")
+				logging.info(self.streamer+" is online. Stream recording in session.")
 
 				if self.osSleep:
 					self.osSleep.inhibit()
@@ -179,39 +172,39 @@ class TwitchRecorder:
 
 				# clean filename from unnecessary characters
 				filename = "".join(x for x in filename if x.isalnum() or x in [" ", "-", "_", "."])
-				
+
 				recordingFilename = os.path.join(self.recordingPath, filename)
-				
+
 				# start streamlink process
 				subprocess.call(["streamlink", "--twitch-disable-hosting", "--twitch-oauth-token", self.OAuthToken, "twitch.tv/" + self.streamer, self.quality, "-o", recordingFilename])
 
-				print("Recording stream is done.")
-				print("Moving file...")
+				logging.info("Recording stream is done.")
+				logging.info("Moving file...")
 				try:
 					os.rename(recordingFilename, os.path.join(self.recordedPath, filename))
 				except:
-					print("Error when moving file.")
+					logging.error("Error when moving file.")
 				if self.fixVideos:
-					print("Fixing video file.")
+					logging.info("Fixing video file.")
 					if(os.path.exists(recordedFilename) is True):
 						try:
 							subprocess.call([self.ffmpegPath, '-err_detect', 'ignore_err', '-i', recordedFilename, '-c', 'copy', os.path.join(self.processedPath, filename)])
 							os.remove(recordedFilename)
 						except Exception as e:
-							print(e)
+							logging.error(e)
 					else:
-						print("Skip fixing. File not found.")
-					print("Fixing is done.")
+						logging.info("Skip fixing. File not found.")
+					logging.info("Fixing is done.")
 
 				if self.osSleep:
 					self.osSleep.uninhibit()
 
-				print("Going back to checking...")
+				logging.info("Going back to checking...")
 				time.sleep(self.refresh)
 
 def main(argv):
 	twitchRecorder = TwitchRecorder()
-	
+
 	args = configargparse.ArgParser(default_config_files=['twitch-recorder.conf'], description="Record automatically Twitch streams.\r\n")
 	args.add("-c", "--config", dest='config_file', is_config_file=True, default='twitch-recorder.conf', type=str)
 	args.add("-o", "--oauth-token", help="OAuth Token from your Twitch account.")
@@ -224,14 +217,16 @@ def main(argv):
 	args.add("-ma", "--mac-address", help="Recorder MAC address.")
 	args.add("-m", "--mode", choices=["recorder", "watcher"], default="recorder", help="Mode to launch the script.")
 	args.add("-q", "--quality", help="Specify the quality to record (examples: best, worst, audio_only, 720p60, 1080p).")
+	args.add("-v", "--verbose", help="Debug mode.", action='store_true')
 	options = args.parse_args()
-	
-	print(options)
-	print("----------")
-	print(args.format_values())
-	print("----------")
-	print("Version:", twitchRecorder.version)
-	
+
+	if options.verbose:
+		logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] v'+twitchRecorder.version+' %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+		logging.debug(options)
+		logging.debug(args.format_values())
+	else:
+		logging.basicConfig(level=logging.INFO, format='v'+twitchRecorder.version+' %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 	while options.streamer == None or options.streamer == "":
 		options.streamer = input("Please specify the streamer to watch: ")
 	twitchRecorder.streamer = options.streamer
@@ -246,7 +241,7 @@ def main(argv):
 	twitchRecorder.rootPath = options.path
 	twitchRecorder.recorderIPAddress = options.ip_address
 	twitchRecorder.recorderMACAddress = options.mac_address
- 
+
 	twitchRecorder.run()
 
 if __name__ == "__main__":
